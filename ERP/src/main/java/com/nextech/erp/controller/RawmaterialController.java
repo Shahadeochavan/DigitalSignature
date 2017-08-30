48d308aafc60590810a0d621c00e78ec712ffda1
nikhil <nikhilraskar50@gmail.com>
2017-08-30 12:58:21 -0700

    formating

--- ERP/src/main/java/com/nextech/erp/controller/RawmaterialController.java
+++ ERP/src/main/java/com/nextech/erp/controller/RawmaterialController.java
@@ -36,44 +36,46 @@ import com.nextech.erp.service.RawmaterialService;
 import com.nextech.erp.service.RawmaterialinventoryService;
 import com.nextech.erp.status.UserStatus;
 import com.nextech.erp.util.ImageUploadUtil;
+
 @Controller
-@Transactional @RequestMapping("/rawmaterial")
+@Transactional
+@RequestMapping("/rawmaterial")
 public class RawmaterialController {
 
 	@Autowired
 	RawmaterialService rawmaterialService;
-	
+
 	@Autowired
 	RawmaterialinventoryService rawmaterialinventoryService;
 
 	@Autowired
 	private MessageSource messageSource;
 
-	@Transactional @RequestMapping(value = "/create",headers = "Content-Type=*/*", method = RequestMethod.POST)
-	public @ResponseBody UserStatus addRawmaterial(
-			HttpServletRequest request,@RequestParam(value = "file", required = false) MultipartFile inputFile,
-			@RequestParam("rmName") String rmName,
-			@RequestParam("partNumber") String partNumber,@RequestParam("description")String description,
-			@RequestParam("pricePerUnit") float pricePerUnit,
-			@RequestParam("unitId") long unitId,@RequestParam("rmTypeId") long rmTypeId ) {
+	@Transactional
+	@RequestMapping(value = "/create", headers = "Content-Type=*/*", method = RequestMethod.POST)
+	public @ResponseBody UserStatus addRawmaterial(HttpServletRequest request,
+			@RequestParam(value = "file", required = false) MultipartFile inputFile,
+			@RequestParam("rmName") String rmName, @RequestParam("partNumber") String partNumber,
+			@RequestParam("description") String description, @RequestParam("pricePerUnit") float pricePerUnit,
+			@RequestParam("unitId") long unitId, @RequestParam("rmTypeId") long rmTypeId) {
 		try {
 			String destinationFilePath = ImageUploadUtil.imgaeUpload(inputFile);
-			  RawMaterialDTO rawMaterialDTO =  new RawMaterialDTO();
-			  rawMaterialDTO.setRmName(rmName);
-			  rawMaterialDTO.setDescription(description);
-			  rawMaterialDTO.setDesign(destinationFilePath);
-			  rawMaterialDTO.setPricePerUnit(pricePerUnit);
-			  rawMaterialDTO.setPartNumber(partNumber);
-			   Unit unit =  new Unit();
-			   unit.setId(unitId);
-			   rawMaterialDTO.setUnitId(unit);
-			    Rmtype rmtype =  new Rmtype();
-			   rmtype.setId(rmTypeId);
-			   rawMaterialDTO.setRmTypeId(rmtype);
-				long id = rawmaterialService.addEntity(RMRequestResponseFactory.setRawMaterial(rawMaterialDTO, request));
-				rawMaterialDTO.setId(id);
-				addRMInventory(rawMaterialDTO, Long.parseLong(request.getAttribute("current_user").toString()));
-				return new UserStatus(1, messageSource.getMessage(ERPConstants.RAW_MATERAIL_ADD, null, null));
+			RawMaterialDTO rawMaterialDTO = new RawMaterialDTO();
+			rawMaterialDTO.setRmName(rmName);
+			rawMaterialDTO.setDescription(description);
+			rawMaterialDTO.setDesign(destinationFilePath);
+			rawMaterialDTO.setPricePerUnit(pricePerUnit);
+			rawMaterialDTO.setPartNumber(partNumber);
+			Unit unit = new Unit();
+			unit.setId(unitId);
+			rawMaterialDTO.setUnitId(unit);
+			Rmtype rmtype = new Rmtype();
+			rmtype.setId(rmTypeId);
+			rawMaterialDTO.setRmTypeId(rmtype);
+			long id = rawmaterialService.addEntity(RMRequestResponseFactory.setRawMaterial(rawMaterialDTO, request));
+			rawMaterialDTO.setId(id);
+			addRMInventory(rawMaterialDTO, Long.parseLong(request.getAttribute("current_user").toString()));
+			return new UserStatus(1, messageSource.getMessage(ERPConstants.RAW_MATERAIL_ADD, null, null));
 		} catch (ConstraintViolationException cve) {
 			cve.printStackTrace();
 			return new UserStatus(0, cve.getCause().getMessage());
@@ -85,70 +87,74 @@ public class RawmaterialController {
 			return new UserStatus(0, e.getCause().getMessage());
 		}
 	}
-	@Transactional @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+
+	@Transactional
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
 	public @ResponseBody UserStatus getRawmaterial(@PathVariable("id") long id) {
 		RawMaterialDTO rawmaterial = null;
 		try {
 			rawmaterial = rawmaterialService.getRMDTO(id);
-			if(rawmaterial==null){
-				return new UserStatus(1,"There is no any rm");
+			if (rawmaterial == null) {
+				return new UserStatus(1, "There is no any rm");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return new UserStatus(1,rawmaterial);
+		return new UserStatus(1, rawmaterial);
 	}
 
-	@Transactional @RequestMapping(value = "/update",headers = "Content-Type=*/*", method = RequestMethod.POST)
-	public @ResponseBody UserStatus upddateRawmaterial(
-			HttpServletRequest request,@RequestParam("file") MultipartFile inputFile,
-			@RequestParam("rmName") String rmName,@RequestParam("id") long id,
-			@RequestParam("partNumber") String partNumber,@RequestParam("description")String description,
-			@RequestParam("pricePerUnit") float pricePerUnit,
-			@RequestParam("unitId") long unitId,@RequestParam("rmTypeId") long rmTypeId ) {
+	@Transactional
+	@RequestMapping(value = "/update", headers = "Content-Type=*/*", method = RequestMethod.POST)
+	public @ResponseBody UserStatus upddateRawmaterial(HttpServletRequest request,
+			@RequestParam("file") MultipartFile inputFile, @RequestParam("rmName") String rmName,
+			@RequestParam("id") long id, @RequestParam("partNumber") String partNumber,
+			@RequestParam("description") String description, @RequestParam("pricePerUnit") float pricePerUnit,
+			@RequestParam("unitId") long unitId, @RequestParam("rmTypeId") long rmTypeId) {
 		try {
 			String destinationFilePath = ImageUploadUtil.imgaeUpload(inputFile);
-			  RawMaterialDTO rawMaterialDTO =  new RawMaterialDTO();
-			  rawMaterialDTO.setRmName(rmName);
-			  rawMaterialDTO.setDescription(description);
-			  rawMaterialDTO.setDesign(destinationFilePath);
-			  rawMaterialDTO.setPricePerUnit(pricePerUnit);
-			  rawMaterialDTO.setPartNumber(partNumber);
-			  rawMaterialDTO.setId(id);
-			   Unit unit =  new Unit();
-			   unit.setId(unitId);
-			   rawMaterialDTO.setUnitId(unit);
-			    Rmtype rmtype =  new Rmtype();
-			   rmtype.setId(rmTypeId);
-			   rawMaterialDTO.setRmTypeId(rmtype);
+			RawMaterialDTO rawMaterialDTO = new RawMaterialDTO();
+			rawMaterialDTO.setRmName(rmName);
+			rawMaterialDTO.setDescription(description);
+			rawMaterialDTO.setDesign(destinationFilePath);
+			rawMaterialDTO.setPricePerUnit(pricePerUnit);
+			rawMaterialDTO.setPartNumber(partNumber);
+			rawMaterialDTO.setId(id);
+			Unit unit = new Unit();
+			unit.setId(unitId);
+			rawMaterialDTO.setUnitId(unit);
+			Rmtype rmtype = new Rmtype();
+			rmtype.setId(rmTypeId);
+			rawMaterialDTO.setRmTypeId(rmtype);
 			rawmaterialService.updateEntity(RMRequestResponseFactory.setRawMaterialUpdate(rawMaterialDTO, request));
-			return new UserStatus(1,messageSource.getMessage(ERPConstants.RAW_MATERAIL_UPDATE, null, null));
+			return new UserStatus(1, messageSource.getMessage(ERPConstants.RAW_MATERAIL_UPDATE, null, null));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new UserStatus(0, e.toString());
 		}
 	}
 
-	@Transactional @RequestMapping(value = "/list", method = RequestMethod.GET, headers = "Accept=application/json")
+	@Transactional
+	@RequestMapping(value = "/list", method = RequestMethod.GET, headers = "Accept=application/json")
 	public @ResponseBody UserStatus getRawmaterial() {
 		List<RawMaterialDTO> rawmaterialList = null;
 		try {
 			rawmaterialList = rawmaterialService.getRMList();
 			if (rawmaterialList.isEmpty()) {
-				return  new UserStatus(1,"There is no any rm list");
+				return new UserStatus(1, "There is no any rm list");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return new UserStatus(1,rawmaterialList);
+		return new UserStatus(1, rawmaterialList);
 	}
 
-	@Transactional @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+	@Transactional
+	@RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
 	public @ResponseBody UserStatus deleteRawmaterial(@PathVariable("id") long id) {
 		try {
-			RawMaterialDTO rawMaterialDTO =rawmaterialService.deleteRM(id);
-			if(rawMaterialDTO==null){
-				return new UserStatus(1,"There is no any rm");
+			RawMaterialDTO rawMaterialDTO = rawmaterialService.deleteRM(id);
+			if (rawMaterialDTO == null) {
+				return new UserStatus(1, "There is no any rm");
 			}
 			return new UserStatus(1, messageSource.getMessage(ERPConstants.RAW_MATERAIL_DELETE, null, null));
 		} catch (Exception e) {
@@ -156,64 +162,70 @@ public class RawmaterialController {
 		}
 	}
 
-	@Transactional @RequestMapping(value = "/getRMaterial/{VendorId}", method = RequestMethod.GET, headers = "Accept=application/json")
+	@Transactional
+	@RequestMapping(value = "/getRMaterial/{VendorId}", method = RequestMethod.GET, headers = "Accept=application/json")
 	public @ResponseBody UserStatus getRawmaterialForVendor(@PathVariable("VendorId") long id) {
 		List<RMVendorAssociationDTO> rawmaterialvendorassociationList = null;
 		try {
 			rawmaterialvendorassociationList = rawmaterialService.getRawmaterialByVenodrId(id);
-			if(rawmaterialvendorassociationList.isEmpty()){
-				return  new UserStatus(1,"There is no any rm assocition list");
+			if (rawmaterialvendorassociationList.isEmpty()) {
+				return new UserStatus(1, "There is no any rm assocition list");
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return new UserStatus(1,rawmaterialvendorassociationList);
+		return new UserStatus(1, rawmaterialvendorassociationList);
 	}
 
-	@Transactional @RequestMapping(value = "/getRMForRMOrder/{RMOrderId}", method = RequestMethod.GET, headers = "Accept=application/json")
+	@Transactional
+	@RequestMapping(value = "/getRMForRMOrder/{RMOrderId}", method = RequestMethod.GET, headers = "Accept=application/json")
 	public @ResponseBody UserStatus getRawmaterialForRMOrder(@PathVariable("RMOrderId") long id) {
 		List<RawMaterialDTO> rawmaterialList = null;
 		try {
 			rawmaterialList = rawmaterialService.getRawMaterialByRMOrderId(id);
-			if(rawmaterialList.isEmpty()){
-				return  new UserStatus(1,"There is no any rm list");
+			if (rawmaterialList.isEmpty()) {
+				return new UserStatus(1, "There is no any rm list");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return new UserStatus(1,rawmaterialList);
+		return new UserStatus(1, rawmaterialList);
 	}
 
-	private void addRMInventory(RawMaterialDTO rawMaterialDTO,long userId) throws Exception{
-		rawmaterialinventoryService.addEntity(RMRequestResponseFactory.setRMIn(rawMaterialDTO,userId));
+	private void addRMInventory(RawMaterialDTO rawMaterialDTO, long userId) throws Exception {
+		rawmaterialinventoryService.addEntity(RMRequestResponseFactory.setRMIn(rawMaterialDTO, userId));
 	}
-	
-	@Transactional @RequestMapping(value = "/getRMaterialList/{RMTypeId}", method = RequestMethod.GET, headers = "Accept=application/json")
+
+	@Transactional
+	@RequestMapping(value = "/getRMaterialList/{RMTypeId}", method = RequestMethod.GET, headers = "Accept=application/json")
 	public @ResponseBody UserStatus getRawmaterialForRMType(@PathVariable("RMTypeId") long id) {
 		List<RawMaterialDTO> rawmaterialList = null;
 		try {
 			rawmaterialList = rawmaterialService.getRMByRMTypeId(id);
-			if(rawmaterialList.isEmpty()){
-				return new UserStatus(1,"There is no any rm list");
+			if (rawmaterialList.isEmpty()) {
+				return new UserStatus(1, "There is no any rm list");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return new UserStatus(1,rawmaterialList);
+		return new UserStatus(1, rawmaterialList);
 	}
-	@Transactional @RequestMapping(value = "/image/{RM-ID}", method = RequestMethod.GET, headers = "Accept=application/json")
-	public @ResponseBody ResponseEntity<byte[]> getRawmaterailImage(@PathVariable("RM-ID") long id,HttpServletRequest request){
+
+	@Transactional
+	@RequestMapping(value = "/image/{RM-ID}", method = RequestMethod.GET, headers = "Accept=application/json")
+	public @ResponseBody ResponseEntity<byte[]> getRawmaterailImage(@PathVariable("RM-ID") long id,
+			HttpServletRequest request) {
 		try {
 			Rawmaterial rawmaterial = rawmaterialService.getRMByRMId(id);
 			String FILE_PATH = rawmaterial.getDesign();
-			
+
 			InputStream in = request.getServletContext().getResourceAsStream(FILE_PATH);
 			in = new FileInputStream(new File(FILE_PATH));
-		    final HttpHeaders headers = new HttpHeaders();
-		    headers.setContentType(MediaType.IMAGE_PNG);
+			final HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.IMAGE_PNG);
 
-		    return new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+			return new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
