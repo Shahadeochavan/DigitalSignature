e1b5814df038d236a97b0e20b73c95d8971edad0
shahadeo <shahadeochavan@gmail.com>
2017-09-19 18:45:56 +0530

    test

--- ERP/src/main/java/com/nextech/erp/serviceImpl/ProductinventoryhistoryServiceImpl.java
+++ ERP/src/main/java/com/nextech/erp/serviceImpl/ProductinventoryhistoryServiceImpl.java
@@ -1,7 +1,6 @@
 package com.nextech.erp.serviceImpl;
 
 import org.springframework.stereotype.Service;
-
 import com.nextech.erp.model.Productinventoryhistory;
 import com.nextech.erp.service.ProductinventoryhistoryService;
 @Service
--- ERP/src/main/java/com/nextech/erp/serviceImpl/ProductionplanningServiceImpl.java
+++ ERP/src/main/java/com/nextech/erp/serviceImpl/ProductionplanningServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
