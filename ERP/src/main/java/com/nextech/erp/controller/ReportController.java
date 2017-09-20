30580d573aa2232515e34d1f5e89d8c776d3b8a9
nikhil <nikhilraskar50@gmail.com>
2017-09-20 13:43:44 -0700

    Removed deprecated methods.

--- ERP/src/main/java/com/nextech/erp/controller/ReportController.java
+++ ERP/src/main/java/com/nextech/erp/controller/ReportController.java
@@ -10,15 +10,13 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.Criteria;
-import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.query.NativeQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.FileCopyUtils;
@@ -53,7 +51,6 @@ import net.sf.dynamicreports.report.builder.datatype.DataTypes;
 import net.sf.dynamicreports.report.constant.HorizontalAlignment;
 import net.sf.dynamicreports.report.exception.DRException;
 
-@SuppressWarnings("deprecation")
 @RestController
 @Transactional @RequestMapping("/report")
 public class ReportController {
@@ -134,7 +131,7 @@ public class ReportController {
 	}
 
 	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@SuppressWarnings("rawtypes")
 	@Transactional @RequestMapping(value = "/inputParameters/{id}", method = RequestMethod.GET, produces = APPLICATION_SCV, headers = "Accept=application/json")
 	public List<ReportInputDTO> inputParameters(@PathVariable("id") long id, final HttpServletRequest request,
 			final HttpServletResponse response) throws Exception {
@@ -145,15 +142,14 @@ public class ReportController {
 				ReportInputDTO inputDTO = new ReportInputDTO();
 				Reportinputparameter reportinputparameter = reportinputassociation.getReportinputparameter();
 				if (reportinputparameter != null && reportinputparameter.isQueryParameter()) {
-					Query query1 = sessionFactory.getCurrentSession().createSQLQuery(reportinputparameter.getQuery());
-					query1.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
-					List results = query1.list();
+					NativeQuery query1 = sessionFactory.getCurrentSession().createNativeQuery(reportinputparameter.getQuery());
+					List results = query1.getResultList();
 					List<ReportInputDataDTO> dataDTOs = new ArrayList<ReportInputDataDTO>();
 					for (Object object : results) {
-						Map<String, Object> data = (Map<String, Object>) object;
+						Object[] data = (Object[]) object;
 						ReportInputDataDTO dto = new ReportInputDataDTO();
-						dto.setName(data.get("Name") + "");
-						dto.setId(data.get("ID") + "");
+						dto.setName(data[1] + "");
+						dto.setId(data[0] + "");
 						dataDTOs.add(dto);
 					}
 					inputDTO.setData(dataDTOs);
@@ -237,6 +233,7 @@ public class ReportController {
 		return null;
 	}
 	//@Scheduled(initialDelay=10000, fixedRate=60000)
+	@SuppressWarnings("unused")
 	private void executeSchedular(){
 		System.out.println("Executed Scheduled method.");
 	}
