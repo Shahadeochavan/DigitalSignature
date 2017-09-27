858397a8441a8a62cdc837bc6745f767c9cad626
shahadeo <shahadeochavan@gmail.com>
2017-09-27 19:42:13 +0530

    test

--- ERP/src/main/java/com/nextech/erp/controller/DispatchController.java
+++ ERP/src/main/java/com/nextech/erp/controller/DispatchController.java
@@ -5,16 +5,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.persistence.PersistenceException;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
--- ERP/src/main/java/com/nextech/erp/controller/NotificationController.java
+++ ERP/src/main/java/com/nextech/erp/controller/NotificationController.java
@@ -6,7 +6,6 @@ import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.nextech.erp.factory.NotificationRequestResponseFactory;
 import com.nextech.erp.newDTO.NotificationDTO;
 import com.nextech.erp.service.NotificationService;
